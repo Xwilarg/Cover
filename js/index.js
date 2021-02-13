7126609cfc58f2faa2d035ebb8fc8a59f57d1bbc
Xwilarg <xwilarg@yahoo.fr>
2021-02-13 03:52:52 +0100

    Add YouTube embed in the website

---
+++ js/index.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+    let frame = document.getElementById("frame");
+    Array.prototype.slice.call(document.getElementsByClassName("elem")).forEach(e => {
+        e.addEventListener("click", function(e) {
+            e = e.target.tagName === "DIV" ? e.target : e.target.parentNode;
+            let url = "https://www.youtube.com/embed/" + e.id + "?autoplay=1";
+            if (frame.src !== url) {
+                frame.src = url;
+            }
+        });
+    });
+}
